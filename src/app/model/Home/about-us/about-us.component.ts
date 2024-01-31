e09d37491b2c20cbf83e9f8e4b6498eadccffe31
Maria <mapis2321@gmail.com>
2024-01-31 14:23:07 -0500

    Se agregó el contenido de AboutUs, se agregó boostrap al index y se instaló font awesome para los iconos que se van a manejar en el proyecto

--- src/app/model/Home/about-us/about-us.component.ts
+++ src/app/model/Home/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
+
+  router = inject(Router)
+
+  Login(){
+    this.router.navigate(['/Login']);
+  }
+  Signin(){
+    this.router.navigate(['/Signin']);
+  }
 
 }
