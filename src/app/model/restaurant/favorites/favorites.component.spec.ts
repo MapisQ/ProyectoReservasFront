3907995c540c59168f3263e36fc7c816b273b524
Maria <mapis2321@gmail.com>
2024-02-08 20:54:12 -0500

    Se han realizado los test algunos componentes del modelo restaurant

---
+++ src/app/model/restaurant/favorites/favorites.component.spec.ts
@@ -0,0 +1,97 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { HomeComponent } from '../../Home/home/home.component';
+import { FavoritesComponent } from '../favorites/favorites.component';
+import { MyBookingsComponent } from '../my-bookings/my-bookings.component';
+import { AboutUsComponent } from '../../Home/about-us/about-us.component';
+import { RestaurantHomeComponent } from '../restaurant-home/restaurant-home.component';
+
+describe('FavoritesComponent', () => {
+    let component: FavoritesComponent;
+    let fixture: ComponentFixture<FavoritesComponent>;
+
+    beforeEach(async () => {
+    await TestBed.configureTestingModule({
+        declarations: [FavoritesComponent,], 
+        imports: [FormsModule, ReactiveFormsModule, RouterTestingModule.withRoutes([{path:'Home', component: HomeComponent}, {path:'Favorites', component:FavoritesComponent},
+        {path:'MyBookings/:selectedRName/:selectedRImg/:formBooking', component:MyBookingsComponent}, {path:'AboutUs', component:AboutUsComponent},{path:'RestaurantHome', component:RestaurantHomeComponent}])],
+       
+    })
+    .compileComponents();
+        fixture = TestBed.createComponent(FavoritesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('Existe Favorites component', () => {
+        const fixture = TestBed.createComponent(FavoritesComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
+
+    it('Me redirecciona a RestaurantHome (Inicio)', async () => {
+        const link = fixture.debugElement.query(By.css('#linkHome'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+
+        await fixture.whenStable();
+        expect(fixture.componentInstance.router.url).toBe('/RestaurantHome');
+    });
+
+    /* it('Me redirecciona a MyBookings (Mis Reservas)', async () => {
+        const link = fixture.debugElement.query(By.css('#linkMyBookings'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+
+        await fixture.whenStable();
+        expect(fixture.componentInstance.router.url).toBe('/MyBookings');
+    }); */
+
+    it('Me redirecciona a Favorites (mis Favoritos)', async () => {
+        const link = fixture.debugElement.query(By.css('#linkMyFavorites'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+
+        await fixture.whenStable();
+        expect(fixture.componentInstance.router.url).toBe('/Favorites');
+    });
+
+    it('Me redirecciona a Home (Cerrar Sesión)', async () => {
+        const link = fixture.debugElement.query(By.css('#linkLogOut'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+
+        await fixture.whenStable();
+        expect(fixture.componentInstance.router.url).toBe('/Home');
+    });
+
+    it('Me redirecciona a Bookings', async () => {
+        const link = fixture.debugElement.query(By.css('#redirect-bookings'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+
+        await fixture.whenStable();
+        expect(component.selectedComponents).toBeTruthy()
+    });
+
+    it('Me quita el restaurante de favoritos',  async () => {
+        const link = fixture.debugElement.query(By.css('#remove-restaurant'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+        await fixture.whenStable();
+        expect(component.removeToFavorites).toBeTruthy()
+    });
+
+    it('Me redirecciona a AboutUs', async () => {
+        const link = fixture.debugElement.query(By.css('#LinkAboutUs'));
+        link.nativeElement.click();
+        fixture.detectChanges();
+
+        await fixture.whenStable();
+        expect(fixture.componentInstance.router.url).toBe('/AboutUs');
+    });
+
+});
--- src/app/model/restaurant/restaurant-home/restaurant-home.component.spec.ts
+++ src/app/model/restaurant/restaurant-home/restaurant-home.component.spec.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from '../../Home/home/home.component';
 import { FavoritesComponent } from '../favorites/favorites.component';
 import { MyBookingsComponent } from '../my-bookings/my-bookings.component';
 import { BookingsComponent } from '../bookings/bookings.component';
+import { AboutUsComponent } from '../../Home/about-us/about-us.component';
 
 describe('Restaurant-Home-Component', () => {
   let component: RestaurantHomeComponent;
@@ -17,7 +18,7 @@ describe('Restaurant-Home-Component', () => {
     await TestBed.configureTestingModule({
       declarations: [RestaurantHomeComponent,], 
       imports: [FormsModule, ReactiveFormsModule, RouterTestingModule.withRoutes([{path:'Home', component: HomeComponent},  {path:'Favorites', component:FavoritesComponent},
-      {path:'MyBookings/:selectedRName/:selectedRImg/:formBooking', component:MyBookingsComponent},{path:'Bookings/:restaurantName/:restaurantImg', component:BookingsComponent}])]
+      {path:'MyBookings/:selectedRName/:selectedRImg/:formBooking', component:MyBookingsComponent}, {path:'AboutUs', component:AboutUsComponent},{path:'RestaurantHome', component:RestaurantHomeComponent},{path:'Bookings/:restaurantName/:restaurantImg', component:BookingsComponent}])]
     })
     .compileComponents();
     fixture = TestBed.createComponent(RestaurantHomeComponent);
@@ -31,13 +32,66 @@ describe('Restaurant-Home-Component', () => {
     expect(app).toBeTruthy();
   });
 
-  it('Me redirecciona a Home', async () => {
+  it('Me redirecciona a RestaurantHome (Inicio)', async () => {
+    const link = fixture.debugElement.query(By.css('#linkHome'));
+    link.nativeElement.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+    expect(fixture.componentInstance.router.url).toBe('/RestaurantHome');
+  });
+
+ /* it('Me redirecciona a MyBookings (Mis Reservas)', async () => {
+    const link = fixture.debugElement.query(By.css('#linkMyBookings'));
+    link.nativeElement.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+    expect(fixture.componentInstance.router.url).toBe('/MyBookings');
+  }); */
+
+  it('Me redirecciona a Favorites (mis Favoritos)', async () => {
+    const link = fixture.debugElement.query(By.css('#linkMyFavorites'));
+    link.nativeElement.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+    expect(fixture.componentInstance.router.url).toBe('/Favorites');
+  });
+
+  it('Me redirecciona a Home (Cerrar Sesión)', async () => {
     const link = fixture.debugElement.query(By.css('#linkLogOut'));
     link.nativeElement.click();
     fixture.detectChanges();
 
     await fixture.whenStable();
     expect(fixture.componentInstance.router.url).toBe('/Home');
+  });
+
+  it('Agrega Restaurante a favoritos', () => {
+    const link = fixture.debugElement.query(By.css('#favoriteIcon'));
+    link.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(component.addToFavorites).toBeTruthy();
+  });
+
+  it('Me redirecciona a bookings', () => {
+    const link = fixture.debugElement.query(By.css('#bookingIcon'));
+    link.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(component.selectedComponents).toBeTruthy()
+});
+
+it('Me redirecciona a AboutUs', async () => {
+    const link = fixture.debugElement.query(By.css('#LinkAboutUs'));
+    link.nativeElement.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+    expect(fixture.componentInstance.router.url).toBe('/AboutUs');
+
   });
 
 });
